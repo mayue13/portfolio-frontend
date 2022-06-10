b654ceaaa3ff30971c4377cda9a9e1e7345c4311
mayue13 <mayurdahak13@gmail.com>
2022-06-11 00:48:21 +0530

    Navbar complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
-import './App.css';
+import './App.scss';
+
+import {About,Footer,Header,Skills,Work,Testimonials} from './container/index'
+
+import {Navbar} from './components/index'
 
 function App() {
   return (
-    <div className="App">
-      <h1>fsd</h1>
+    <div className="app">
+      <Navbar/>
+      <Header/>
+      <About/>
+      <Work/>
+      <Skills/>
+      <Testimonials/>
+      <Footer/>
     </div>
   );
 }
