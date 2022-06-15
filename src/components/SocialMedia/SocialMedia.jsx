cbbafbdf99ccc9f28d474e4b3f1e0aae39071941
mayue13 <mayurdahak13@gmail.com>
2022-06-15 12:51:16 +0530

    contact

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BsTwitter,BsInstagram, BsGithub} from 'react-icons/bs';
+import {BsTwitter,BsInstagram, BsGithub, BsLinkedin} from 'react-icons/bs';
 import {FaFacebookF } from 'react-icons/fa';
 
 
@@ -7,10 +7,10 @@ export default function SocialMedia (){
 
     return(
         <div className='app__social'>
-            <div><BsTwitter/></div>
-            <div><BsInstagram/></div>
-            <div><BsGithub/></div>
-            <div><FaFacebookF/></div>
+            <div><a href='https://twitter.com/MayueDahake' target="_blank"><BsTwitter /></a></div>
+            <div><a href='https://www.instagram.com/mayue.dahake' target="_blank"><BsInstagram/></a></div>
+            <div><a href='https://github.com/mayue13' target="_blank"><BsGithub/></a></div>
+            <div><a href='https://www.linkedin.com/in/mayurdahake/' target="_blank"><BsLinkedin/></a></div>            
         </div>
 
     )
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,11 +1,113 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
-import './Footer.scss'
+import { urlFor, client } from "../../client";
+import { AppWrap, MotionWrap } from "../../wrapper/index";
+import { images } from "../../constants";
+
+import "./Footer.scss";
 
 const Footer = () => {
-  return (
-    <div></div>
-  )
-}
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-export default Footer
+  const { name, email, message } = formData;
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    setLoading(true);
+
+    const contact = {
+      _type: "contact",
+      name: formData.name,
+      email: formData.email,
+      message: formData.message,
+    };
+
+    client
+      .create(contact)
+      .then(() => {
+        setLoading(false);
+        setIsFormSubmitted(true);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <>
+      <h2 className="head-text">Take a coffe & chat with me</h2>
+
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <img src={images.email} />
+          <a href="mailto:mayurdahake13@gmail.com" className="p-text">
+            mayurdahake13@gmail
+          </a>
+        </div>
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="phone" />
+          <a href="tel:+1 (123) 456-7890" className="p-text">
+            +1 (123) 456-7890
+          </a>
+        </div>
+      </div>
+
+      {!isFormSubmitted?(
+      <div className="app__footer-form app__flex">
+        <div className="app__flex">
+          <input
+            className="p-text"
+            type="text"
+            placeholder="Your Name"
+            name="name"
+            value={name}
+            onChange={handleChangeInput}
+          />
+        </div>
+        <div className="app__flex">
+          <input
+            className="p-text"
+            type="email"
+            placeholder="Your Email"
+            name="email"
+            value={email}
+            onChange={handleChangeInput}
+          />
+        </div>
+        <div>
+          <textarea
+            className="p-text"
+            placeholder="Your Message"
+            value={message}
+            name="message"
+            onChange={handleChangeInput}
+          />
+        </div>
+        <button className="p-text" type="button" onClick={handleSubmit}>
+          {loading ? "Sending" : "Send Message"}
+        </button>
+      </div>
+      ):(
+      <div>
+        <h3 className="head-text">Thank you for get in touch</h3>
+      </div>
+      )}
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(Footer, "app__footer"),
+  "contact",
+  "app__whitebg"
+);
