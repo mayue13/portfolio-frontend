eead54accf089511ffc521e20d17ddab93ceef22
mayur krushnarao dahake <35840117+mayue13@users.noreply.github.com>
2022-12-31 01:39:04 +0530

    Update Skills.jsx

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import ReactTooltip from "react-tooltip";
-import { format }  from "date-fns";
+
+import { format, formatDistanceStrict } from "date-fns";
 
 import "./Skills.scss";
 import { AppWrap, MotionWrap } from "../../wrapper/index";
@@ -23,6 +23,55 @@ function Skills() {
       setExperience(data);
     });
   }, []);
+
+  const monthYear = function (date) {
+    if (date == null) return "Present";
+    else var newDate = new Date(date);
+
+    var year = newDate.getFullYear();
+    var month = format(newDate, "LLL");
+
+    return month + " " + year;
+  };
+
+  const workExperince = function (joiningDate, releaseDate) {
+    joiningDate = new Date(joiningDate);
+    releaseDate = releaseDate != null ? new Date(releaseDate) : new Date();
+
+    const date1_time_stamp = joiningDate.getTime();
+    const date2_time_stamp = releaseDate.getTime();
+
+    let calc;
+    if (date1_time_stamp > date2_time_stamp) {
+      calc = new Date(date1_time_stamp - date2_time_stamp);
+    } else {
+      calc = new Date(date2_time_stamp - date1_time_stamp);
+    }
+
+    const calcFormatTmp =
+      calc.getDate() + "-" + (calc.getMonth() + 1) + "-" + calc.getFullYear();
+    const calcFormat = calcFormatTmp.split("-");
+
+    const months_passed = Number(Math.abs(calcFormat[1]) - 1);
+    const years_passed = Number(Math.abs(calcFormat[2]) - 1970);
+
+    const yrsTxt = ["year", "years"];
+    const mnthsTxt = ["month", "months"];
+
+    const result =
+      (years_passed == 1
+        ? years_passed + " " + yrsTxt[0] + " "
+        : years_passed > 1
+        ? years_passed + " " + yrsTxt[1] + " "
+        : "") +
+      (months_passed == 1
+        ? months_passed + " " + mnthsTxt[0]
+        : months_passed > 1
+        ? months_passed + " " + mnthsTxt[1] + " "
+        : "");
+
+    return result;
+  };
 
   return (
     <>
@@ -53,12 +102,9 @@ function Skills() {
 
         <motion.div className="app__skills-exp">
           {experience?.map((experience) => (
-            <motion.div 
-              className="app__skills-exp-item" 
-              key={experience.year}
-            >
+            <motion.div className="app__skills-exp-item" key={experience.year}>
               <div className="app__skills-exp-year">
-                 <p className="bold-text">{experience.company}</p>
+                <p className="bold-text">{experience.company}</p>
               </div>
 
               <motion.div className="app__skills-exp-works">
@@ -73,7 +119,9 @@ function Skills() {
                       key={work.name}
                     >
                       <h4 className="bold-text">{work.name}</h4>
-                      <p className="p-text">{work.joiningDate} - {work.releaseDate}</p>
+                      <p className="p-text">
+                        {monthYear(work.joiningDate)} - {monthYear(work.releaseDate)} | {workExperince(work.joiningDate,work.releaseDate)}
+                      </p>                      
                     </motion.div>
                     {/* <ReactTooltip
                       id={work.name}
@@ -97,7 +145,7 @@ function Skills() {
 //export default AppWrap(Skills, "skills");
 
 export default AppWrap(
-  MotionWrap(Skills,'app__skills'),
-  'skills',
+  MotionWrap(Skills, "app__skills"),
+  "skills",
   "app__whitebg"
-  ); 
+);
